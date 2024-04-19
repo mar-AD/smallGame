ddaff9eb0ed4cb2b9c57bb236ae72e8308695605
marra abdallah <abdallah4marra@gmail.com>
2024-04-19 16:59:00 +0100

    finnal

--- index.js
+++ index.js
@@ -1,153 +1,126 @@
 
-
 let sezurs = document.querySelector(".img");
 let place = document.querySelector(".middle");
 let result = document.querySelector(".results");
 let btn = document.querySelector(".btn");
 let imgs = document.querySelectorAll(".imgg");
-let imgcont= document.querySelector(".left")
-let forcump = document.querySelector(".forcump")
-let foruse = document.querySelector(".foruse")
+let imgcont = document.querySelector(".left");
+let forcump = document.querySelector(".forcump");
+let foruse = document.querySelector(".foruse");
 
-let arrrray = [  
+let arrrray = [
     'scissors.png',
     'rock.png',
     'paper.png'
-] 
-
+];
 
 let computerPick;
 let counter = 0;
-let countClick=0;
+let countClick = 0;
 let countuser = 0;
 let countcomp = 0;
+let rounds = 0;
 
-btn.addEventListener("click", ()=>{
-    computerPick = arrrray[getComputerChoice()];
-    if (counter<arrrray.length) {
-        counter ++;   
-        sezurs.classList.add("none");
-        // result.classList.add("nonee")
-    }else{
-        counter = 0;
-        
+btn.addEventListener("click", () => {
+    rounds++;
+    console.log("Round:", rounds);
+    if (rounds <= 5) {
+        btn.disabled = true; 
+        computerPick = arrrray[getComputerChoice()];
+        if (counter < arrrray.length) {
+            counter++;
+            sezurs.classList.add("none");
+        } else {
+            counter = 0;
+        }
+    } else {
+        displayWinner();
     }
-    // seting a timeout to reload every 4s--------------------
-    // countClick++;
-    // console.log(countClick)
-    // if (countClick>=5) {
-    //     location.reload()
-        
-    // }
-    
-    // setTimeout(() => {
-    //     location.reload()
-    //  }, 4000);
-    
-})
-// getting a random value ---------------------------
+});
 
-function getComputerChoice (){
+function getComputerChoice() {
     const i = Math.floor(Math.random() * arrrray.length);
     return i;
 }
 
-
-// the possible conditons ------------------------
-
 for (let i = 0; i < imgs.length; i++) {
     const element = imgs[i];
-    element.addEventListener("click", (t)=>{
-    const tar = t.target;
-    const userPick = tar.src.substring(tar.src.lastIndexOf('/') + 1);
-    console.log(tar)
-    
-    countClick++;
+    element.addEventListener("click", (t) => {
+        const tar = t.target;
+        const userPick = tar.src.substring(tar.src.lastIndexOf('/') + 1);
+        countClick++;
 
-
-
-    if (userPick=== computerPick) {
-        setTimeout(() => {
-            sezurs.src=computerPick;
-        }, 500);
-        place.innerHTML="you are even, try again";
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else if (userPick=== 'scissors.png' && computerPick==='rock.png'){
-        setTimeout(() => {
-            sezurs.src=computerPick;
-            place.innerHTML="the computer won";
-            sezurs.classList.add("green");
-            element.classList.add("red");
-        }, 500);
-        
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else if (userPick=== 'scissors.png' && computerPick==='paper.png'){
-        setTimeout(() => {
-            sezurs.src=computerPick;
-            place.innerHTML="the user won";
-            element.classList.add("green");
-            sezurs.classList.add("red");
-        }, 500);
-        
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else if (userPick=== 'rock.png' && computerPick==='paper.png'){
-        setTimeout(() => {
-            sezurs.src=computerPick;
-            place.innerHTML="the computer won";
-            sezurs.classList.add("green");
-            element.classList.add("red");
-        }, 500);
-        
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else if (userPick=== 'rock.png' && computerPick==='scissors.png'){
-        setTimeout(() => {
-            sezurs.src=computerPick;
-            place.innerHTML="the user won";
-            element.classList.add("green");
-            sezurs.classList.add("red");
-        }, 500);
-        
-        
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else if (userPick=== 'paper.png' && computerPick==='scissors.png'){
-        setTimeout(() => {
-            sezurs.src=computerPick;
-            place.innerHTML="the computer won";
-            sezurs.classList.add("green");
-            element.classList.add("red");
-        }, 500);
-        
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else if (userPick=== 'paper.png' && computerPick==='rock.png'){
-        setTimeout(() => {
-            sezurs.src=computerPick;
-            place.innerHTML="the user won";
-            element.classList.add("green");
-            sezurs.classList.add("red");
-        }, 500);
-        
-        element.classList.add('none');
-        imgcont.classList.add("flep");
-    }else{
-        sezurs.src=computerPick;
-        place.innerHTML="this is wrong";
-        
-    }
-    if (countClick == 5) {
-        setTimeout(() => {
-        location.reload()
-    }, 4000);
-    }
-    
-    
-})
+        if (userPick === computerPick) {
+            setTimeout(() => {
+                sezurs.src = computerPick;
+            }, 500);
+            place.innerHTML = "You are even, try again";
+            element.classList.add('none');
+            imgcont.classList.add("flep");
+            setTimeout(() => {
+                resetCards();
+            }, 2000);
+        } else if ((userPick === 'scissors.png' && computerPick === 'rock.png') ||
+            (userPick === 'rock.png' && computerPick === 'paper.png') ||
+            (userPick === 'paper.png' && computerPick === 'scissors.png')) {
+            setTimeout(() => {
+                sezurs.src = computerPick;
+                place.innerHTML = "The computer won";
+                sezurs.classList.add("green");
+                element.classList.add("red");
+            }, 500);
+            countcomp++;
+            forcump.innerHTML = countcomp;
+            element.classList.add('none');
+            imgcont.classList.add("flep");
+            setTimeout(() => {
+                resetCards();
+            }, 2000);
+        } else {
+            setTimeout(() => {
+                sezurs.src = computerPick;
+                place.innerHTML = "The user won";
+                element.classList.add("green");
+                sezurs.classList.add("red");
+            }, 500);
+            countuser++;
+            foruse.innerHTML = countuser;
+            element.classList.add('none');
+            imgcont.classList.add("flep");
+            setTimeout(() => {
+                resetCards();
+            }, 2000);
+        }
+        if (rounds === 5) {
+            displayWinner();
+        }
+    });
 }
 
-        
+function resetCards() {
+    console.log("Resetting cards...");
+    for (let img of imgs) {
+        img.classList.remove('none');
+        imgcont.classList.remove('flep');
+        img.classList.remove('green', 'red');
+        sezurs.classList.remove('none', 'green', 'red');
+        sezurs.src = '';
+        place.innerHTML = '';
+    }
+    btn.disabled = false;
+}
 
+function displayWinner() {
+    if (countuser > countcomp) {
+        foruse.classList.add("green");
+        place.innerHTML = "USER WON";
+    } else if (countuser < countcomp) {
+        forcump.classList.add("green");
+        place.innerHTML = "COMPUTER WON";
+    } else {
+        place.innerHTML = "It's a tie!";
+    }
+    setTimeout(() => {
+        location.reload();
+    }, 3000);
+}
